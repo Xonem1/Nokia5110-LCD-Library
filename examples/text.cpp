2070175d25c1fc07eeb3b34dd529f5a8be4b610c
drewcassidy <drewcassidy@me.com>
2017-01-04 20:15:15 -0800

    center "hello world" in text example

--- examples/text.cpp
+++ examples/text.cpp
@@ -8,7 +8,7 @@ int main() {
     display.print_string("ABCDEFGHIJKLMN", 0, 0);
     display.print_string("abcdefghijklmn", 0, 8);
     display.print_string("!?@#$%^&*+()[]", 0, 16);
-    display.fill_rrect(2, 28, 77, 38, 2);
-    display.print_string("Hello World!", 4, 30, Nokia5110::pixel_xor);
+    display.fill_rrect(5, 28, 79, 38, 2);
+    display.print_string("Hello World!", 8, 30, Nokia5110::pixel_xor);
     display.display();
 }
